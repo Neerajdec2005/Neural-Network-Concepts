e788a462a96503707a2868128599e77065a609d8
Neerajdec2005 <neeraj.coder04@gmail.com>
2026-02-07 16:15:38 +0530

    adding loops instead of raw implementation

--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,18 @@
 _in= [1.3, 3.2, 4.5, 7.2]
-weights1= [1.2 ,5.7, 3.1, 2.4]
-weights2= [1.7 ,4.5, 3.7, 1.9]
-weights3= [4.1 ,2.2, 5.1, 2.9]
 
-bias1=4
-bias2=2.4
-bias3=3.4
+weights=[[1.2 ,5.7, 3.1, 2.4],
+         [1.7 ,4.5, 3.7, 1.9],
+         [4.1 ,2.2, 5.1, 2.9]]
 
-output=[_in[0]*weights1[0] + _in[1]*weights1[1] + _in[2]*weights1[2] + _in[3]*weights1[3] + bias1,
-        _in[0]*weights2[0] + _in[1]*weights2[1] + _in[2]*weights2[2] + _in[3]*weights2[3] + bias2,
-        _in[0]*weights3[0] + _in[1]*weights3[1] + _in[2]*weights3[2] + _in[3]*weights3[3] + bias3]
+bias=[4, 2.4, 3.4]
 
-print(output)
+layer_outputs=[]
+
+for i in range(len(weights)):
+    neuron_output=0
+    for j in range(len(_in)):
+        neuron_output+=_in[j]*weights[i][j]
+    neuron_output+=bias[i]
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
