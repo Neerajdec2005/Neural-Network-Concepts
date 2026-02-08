7e77e193af544491fe8f38925645eae78d7a2783
Neerajdec2005 <neeraj.coder04@gmail.com>
2026-02-08 20:23:56 +0530

    updating softmax to handle batches

--- softmax.py
+++ softmax.py
@@ -1,19 +1,18 @@
-import math
 import numpy as np
 
-E= math.e
-
-layer_outputs= [1.4, -2.15, -4, 3]
+layer_outputs= [[1.4, -2.15, -4, 3],
+                [-1.2, 2.3, 0.8, -0.5],
+                [0.1, -1.2, 3.4, -0.7]]
 
 exp=np.exp(layer_outputs)
 
 print(exp)
 print()
 
-norm_base= sum(exp)
+norm_base= np.sum(exp, axis=1, keepdims=True)
 
 softmax= exp/ norm_base
 print(softmax)
 print()
-print(sum(softmax))
+print(np.sum(softmax, axis=1))
 
